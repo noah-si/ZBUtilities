42f3e16044bb0874a2c2bec6684c39b7c3a63921
Bin <30135878@qq.com>
2013-08-02 21:03:58 +0800

    Add classes

---
+++ NSString+ChineseSpace.h
@@ -0,0 +1,15 @@
+//
+//  NSString+ChineseSpace.h
+//  BookReader
+//
+//  Created by zhangbin on 7/6/13.
+//  Copyright (c) 2013 ZoomBin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (ChineseSpace)
+
++ (NSString *)ChineseSpace;
+
+@end
