1a1152ff33c63f88272e2dea7a949b3ec153559f
Bin <30135878@qq.com>
2013-08-17 14:31:13 +0800

    Add +is4Inch method

---
+++ UIDevice+ZBUtilites.h
@@ -0,0 +1,15 @@
+//
+//  UIDevice+ZBUtilites.h
+//  BookReader
+//
+//  Created by zhangbin on 8/17/13.
+//  Copyright (c) 2013 ZoomBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (ZBUtilites)
+
+- (BOOL)is4Inch;
+
+@end
---
+++ UIDevice+ZBUtilites.m
@@ -0,0 +1,18 @@
+//
+//  UIDevice+ZBUtilites.m
+//  BookReader
+//
+//  Created by zhangbin on 8/17/13.
+//  Copyright (c) 2013 ZoomBin. All rights reserved.
+//
+
+#import "UIDevice+ZBUtilites.h"
+
+@implementation UIDevice (ZBUtilites)
+
+- (BOOL)is4Inch
+{
+	return ( [UIScreen mainScreen].bounds.size.height == 568 );
+}
+
+@end
