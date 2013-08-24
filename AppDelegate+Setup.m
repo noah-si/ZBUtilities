efa5aac9744b5b83a2a8ba1aaaf50a34319771ad
Bin <30135878@qq.com>
2013-08-24 14:57:53 +0800

    Add AppDelegate category

---
+++ AppDelegate+Setup.m
@@ -0,0 +1,15 @@
+//
+//  AppDelegate+Setup.m
+//  BookReader
+//
+//  Created by zhangbin on 8/24/13.
+//  Copyright (c) 2013 ZoomBin. All rights reserved.
+//
+
+#import "AppDelegate+Setup.h"
+
+
+
+@implementation AppDelegate (Setup)
+
+@end
