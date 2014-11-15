61d90ec85fafbb7c301e83425f0eb6a0924af8d7
Bin Zhang <30135878@qq.com>
2014-11-15 15:26:02 +0800

    Add UITableViewCell category.

---
+++ UITableViewCell+ZBUtilities.h
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+ZBUtilities.h
+//  dushuhu
+//
+//  Created by zhangbin on 11/15/14.
+//  Copyright (c) 2014 zoombin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UITableViewCell (ZBUtilities)
+
++ (NSString *)identifier;
+
+@end
